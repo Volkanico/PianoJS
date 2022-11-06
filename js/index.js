4eb175398c641179fee680dab0d382ea8339db1f
Volkanico <vcekmez15181@alumnes.iesmanacor.cat>
2022-11-06 16:52:12 +0100

    Maquetacio HTML i CSS de Partitures OK

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.log("HOLAAA")
+
+
+
+//let todosLosAudios = [];
+let nomNota;
+let tipusNota = false;
+let cerca = [];
+function nota(nomNota, tipusNota) {
+        this.nomNota = nomNota;
+        this.tipusNota = tipusNota;
+}
+
+function addCerca (nomNota,tipusNota){
+    cerca.push(new nota(nomNota,tipusNota));
+}
+
+/*
+function afegirAudio(){
+   for(let i = 0; i <  )
+    todosLosAudios.push(new Audio("../audio/do-sust.mp3"));
+}
+afegirAudio();*/
+console.log(todosLosAudios);
