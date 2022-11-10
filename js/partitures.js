cc6ede1720e30010c9a572cd7b9cf4bec44878de
Volkanico <vcekmez15181@alumnes.iesmanacor.cat>
2022-11-10 21:14:03 +0100

    Tema 2 acabat, nomes falta boto esborrar

--- js/partitures.js
+++ js/partitures.js
@@ -1,13 +1,72 @@
-let table = '';
-table += '<table>';
-table += '<tr>';
-table += '<th>Nom</th>';
-table += '<th>Es Perillos</th>';
-table += '<th>Distancia</th>';
-table += '<th>Cuerpo que orbita</th>';
-table += '<th>Velocidad KM/H</th>';
-table += '</tr>';
-table += '</table>';
+const santAntoniRegistre = ['Sant Antoni i el dimoni', 'ca',
+    '<button><a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>Editar</a></button><button onclick="obrirPrompt()"><a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>Esborrar</a></button>'
+];
+const laBalangueraRegistre = ['La Balanguera', 'ca',
+    '<button><a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>Editar</a></button><button onclick="obrirPrompt()"><a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>Esborrar</a></button>'
+];
+const merryChristmasRegistre = ["Merry Christmas", 'en',
+    '<button><a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>Editar</a></button><button onclick="obrirPrompt()"><a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>Esborrar</a></button>'
+];
+const frereJacquesRegistre = ["Frère Jacques", 'ca',
+    '<button><a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>Editar</a></button><button onclick="obrirPrompt()"><a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>Esborrar</a></button>'
+];
+const registreCanconcsDe100 = [];
+
+function registresArr() {
+    registreCanconcsDe100.push(laBalangueraRegistre);
+    registreCanconcsDe100.push(merryChristmasRegistre);
+    registreCanconcsDe100.push(frereJacquesRegistre);
+    for (let i = 0; i < 98; i++) {
+        registreCanconcsDe100.push(santAntoniRegistre);
+    }
+}
+
+let j;
+function pintarRegistres() {
+    let table = '';
+    table += '<table>';
+    table += '<tr>';
+    table += '<th>Titol</th>';
+    table += '<th>Idioma original</th>';
+    table += '<th>Accions</th>';
+    table += '</tr>';
+
+    for (j = 0; j < registreCanconcsDe100.length; j++) {
+        let id = '<tr id=' + j + '>'
+        table += id;
+        table += '<td>' + registreCanconcsDe100[j][0] + '</td>';
+        table += '<td>' + registreCanconcsDe100[j][1] + '</td>';
+        table += '<td>' + registreCanconcsDe100[j][2] + '</td>';
+        table += '</tr>';
+    }
+    table += '</table>';
+    document.querySelector("#table").innerHTML = table;
+}
+
+function obrirPrompt() {
 
 
-document.querySelector("#table").innerHTML = table;
+    if (window.confirm("Està segur que vol esborrar l'element? ")) {
+        document.getElementById("#1").remove();
+
+    }
+}
+
+function eliminar(j) {
+
+}
+
+function login() {
+    let ample = window.screen.availWidth;
+let altura = window.screen.availHeight;
+ample = ample / 2 - 300;
+altura = altura / 2 - 200;
+let finestra;
+finestra = window.open("formulari.html","Formulari","width=800,height=500,left=" + ample + ",top=" + altura);
+}
+
+function init() {
+    registresArr();
+    pintarRegistres();
+}
+init();
--- js/piano.js
+++ js/piano.js
@@ -1,28 +1,28 @@
 
 let nomNotesTotals = ["do-sust.mp3", "do-sust.ogg", "do.mp3", "do.ogg", "do7.mp3", "do7.ogg", "fa-sust.mp3", "fa-sust.ogg", "fa.mp3",
-"fa.ogg", "la-sust.mp3", "la-sust.ogg", "la.mp3", "la.ogg", "mi.mp3", "mi.ogg",  "re-sust.mp3", "re-sust.ogg", "re.mp3",
-"re.ogg", "si.mp3", "si.ogg" , "si0.mp3", "si0.ogg", "sol-sust.mp3", "sol-sust.ogg", "sol.mp3", "sol.ogg"];
+    "fa.ogg", "la-sust.mp3", "la-sust.ogg", "la.mp3", "la.ogg", "mi.mp3", "mi.ogg", "re-sust.mp3", "re-sust.ogg", "re.mp3",
+    "re.ogg", "si.mp3", "si.ogg", "si0.mp3", "si0.ogg", "sol-sust.mp3", "sol-sust.ogg", "sol.mp3", "sol.ogg"];
 let imgNotes = ["nota1.png", "nota1sust.png", "nota2.png", "nota2sust.png", "pentagrama.png"];
- 
+
 let nomNota;
 let sostingut = false;
 let cerca = [];
 
 let notesTotals = [];
 
-let laBalanguera = ["do","re","mi","fa","fa","sol","sol","la-sust"];
-let happyBirthday = ["do","do","re","do","fa","mi","do","do","re","do","sol","fa"];
+let laBalanguera = ["do", "re", "mi", "fa", "fa", "sol", "sol", "la-sust"];
+let happyBirthday = ["do", "do", "re", "do", "fa", "mi", "do", "do", "re", "do", "sol", "fa"];
 
-let partitures = [laBalanguera,happyBirthday];
+let partitures = [laBalanguera, happyBirthday];
 
 function Nota(nomNota, sostingut) {
-        this.nomNota = nomNota;
-        this.sostingut = sostingut;      
+    this.nomNota = nomNota;
+    this.sostingut = sostingut;
 }
 
 
-function addCerca (nomNota,sostingut){
-    if(nomNota.substr(-9) === "-sust.mp3" || nomNota.substr(-9) === "-sust.ogg"){
+function addCerca(nomNota, sostingut) {
+    if (nomNota.substr(-9) === "-sust.mp3" || nomNota.substr(-9) === "-sust.ogg") {
         cerca.push(new Nota(nomNota, sostingut === false));
     } else {
         cerca.push(new Nota(nomNota, sostingut === true));
@@ -30,67 +30,18 @@ function addCerca (nomNota,sostingut){
 }
 
 function cercador(partitures) {
-    let subconjORDENAT =  ["fa","mi","do"]; //ENTRADA DE NOTES
+    let subconjORDENAT = ["fa", "mi", "do"]; //ENTRADA DE NOTES
 
-    for(var i = 0; i < partitures.length; i++) { //COMPARADOR
-        for(var j = 0; j < partitures[i].length; j++){
-            if(subconjORDENAT[0] === partitures[i][j] && subconjORDENAT[1] === partitures[i][j + 1] && subconjORDENAT[2] === partitures[i][j + 2]){
+    for (var i = 0; i < partitures.length; i++) { //COMPARADOR
+        for (var j = 0; j < partitures[i].length; j++) {
+            if (subconjORDENAT[0] === partitures[i][j] && subconjORDENAT[1] === partitures[i][j + 1] && subconjORDENAT[2] === partitures[i][j + 2]) {
                 console.log(partitures[i])
-}
             }
         }
     }
-    
-
-    
-          
-
-
-
-
-
-
-
-function guardarNotaANotesTotals (nomNotesTotals) {
-    for(let i = 0; i < nomNotesTotals.length; i++) {
-           addCerca(nomNotesTotals[i], sostingut)
-
-        
-    } 
 }
-
 
 //console.log(cerca);
 //guardarNotaANotesTotals(nomNotesTotals);
 //console.log(cerca);
 cercador(partitures);
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-function cercador () {
-    imgNotes.push("../notesimg/nota1.png");
-    imgNotes.push("../notesimg/nota1sust.png");
-    imgNotes.push("../notesimg/nota2.png");
-    imgNotes.push("../notesimg/nota2sust.png");
-    imgNotes.push("../notesimg/pentagrama.png");
-}
-
-function afegirAudio(){
-   for(let i = 0; i <  )
-    todosLosAudios.push(new Audio("../audio/do-sust.mp3"));
-}
-afegirAudio();
-cercador();
-console.log(imgNotes);
-*/
