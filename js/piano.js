a97e4fc8301d3d1d8b2c3d0c9b03501a45a4d17d
Volkanico <vcekmez15181@alumnes.iesmanacor.cat>
2022-11-10 21:22:32 +0100

    Ja sona el piano! metode nota(oscilacio)

--- js/piano.js
+++ js/piano.js
@@ -41,6 +41,19 @@ function cercador(partitures) {
     }
 }
 
+var context= new AudioContext();
+ 
+function jsNota(frecuencia){
+        var o= context.createOscillator();
+        g=context.createGain();
+        o.connect(g);
+        o.type="sawtooth";
+        o.frequency.value=frecuencia;
+        g.connect(context.destination);
+        o.start(0);
+        g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
+    }
+
 //console.log(cerca);
 //guardarNotaANotesTotals(nomNotesTotals);
 //console.log(cerca);
