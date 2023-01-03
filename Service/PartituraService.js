b6c7d11df8c1f544a455c775403959a1ece1dcee
Volkanico <vcekmez15181@alumnes.iesmanacor.cat>
2023-01-03 19:38:39 +0100

    View Partitures OK

--- Service/PartituraService.js
+++ Service/PartituraService.js
@@ -1,5 +1,54 @@
+"use strict";
 class PartituraService {
     constructor(){
         
     }
 }
+
+
+const santAntoniRegistre = ['Sant Antoni i el dimoni', 'ma',];
+const laBalangueraRegistre = ['La Balanguera', 'ca',];
+const merryChristmasRegistre = ["Merry Christmas", 'en'];
+const frereJacquesRegistre = ["Frère Jacques", 'ca'];
+const registreCanconcsDe100 = [];
+
+function registresArr() {
+    registreCanconcsDe100.push(laBalangueraRegistre);
+    registreCanconcsDe100.push(merryChristmasRegistre);
+    registreCanconcsDe100.push(frereJacquesRegistre);
+    registreCanconcsDe100.push(laBalangueraRegistre);
+    registreCanconcsDe100.push(merryChristmasRegistre);
+    registreCanconcsDe100.push(frereJacquesRegistre);
+    for (let i = 0; i < 98; i++) {
+        registreCanconcsDe100.push(santAntoniRegistre);
+    }
+}
+
+let j;
+function getPartitures() {
+    pintarCapceleraTaula()
+    let taula = document.querySelector("table")
+    for (j = 0; j < registreCanconcsDe100.length; j++) {
+      pintarTaula(taula)
+    }
+
+}
+
+function obrirPrompt(index) {
+    if (window.confirm("Està segur que vol esborrar l'element? " + index)) {
+        alert("S'ha esborrat l'element " + index)
+        document.getElementById(index).remove();
+        //event listener 
+    }else{alert("S'ha cancelat l'acció")}
+}
+
+
+
+
+
+function init() {
+    registresArr();
+    getPartitures();
+  
+}
+init();
---
+++ View/ViewPartitures.js
@@ -0,0 +1,54 @@
+function pintarCapceleraTaula(){
+    let table = `
+    <table>
+    <thead>
+    <tr>
+    <th>Titol</th>
+    <th>Idioma original</th>
+    <th>Accions</th>
+    </tr>
+    </thead>
+    <tbody>
+    </tbody>`;
+    let divTaula = document.querySelector("#table");
+    divTaula.innerHTML = table;
+}
+
+
+function pintarTaula(taula){
+    let tr = document.createElement("tr")
+    taula.appendChild(tr)
+    tr.setAttribute("id",j)
+    let td1 = document.createElement("td")
+    let td2 = document.createElement("td")
+    let td3 = document.createElement("td")
+    tr.appendChild(td1)
+    tr.appendChild(td2)
+    tr.appendChild(td3)
+    td1.textContent = registreCanconcsDe100[j][0] //PINTAR REGISTRE NOU
+    td2.textContent = registreCanconcsDe100[j][1] //PINTAR REGISTRE NOU
+    let basura = document.createElement("button")
+    basura.addEventListener("click", function () {
+        obrirPrompt(tr.getAttribute("id"))
+    });
+
+    let imatgebasura = document.createElement("img")
+    imatgebasura.setAttribute('src', '../icons/pen-to-square-solid.svg')
+    basura.textContent = "Esborrar"
+    basura.appendChild(imatgebasura)
+
+    let editar = document.createElement("button")
+    editar.textContent = "Editar"
+
+    editar.addEventListener("click", function () {
+        obrirPrompt(tr.getAttribute("id"))
+    });
+
+    let imatgeeditar = document.createElement("img")
+    imatgeeditar.setAttribute('src', '../icons/trash-solid.svg')
+    editar.appendChild(imatgeeditar)
+    
+    td3.appendChild(basura)
+    td3.appendChild(editar)
+    document.querySelector('tbody').appendChild(tr);
+}
